92b37f0fd0b92273e9362510f6aa08f1d9770a4c
Basudev Bharat Bhushan <bharatbhushan173@gmail.com>
2021-06-18 01:27:47 +0530

    Level-4 Hashing with Multiple Salting Rounds

--- app.js
+++ app.js
@@ -1,75 +1,87 @@
 //jshint esversion:6
-const express =require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const app = express();
-const md5 = require("md5");
+const bcrypt = require("bcrypt"); //Level 4
+const saltRound = 10; //Level 4
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
-  extended:true
+  extended: true
 }));
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true,useUnifiedTopology:true});
-mongoose.set("useFindAndModify",false);
-mongoose.set("useCreateIndex",true);
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 
-/////////lEVEL-3 Hashing the password/////////
-const userSchema = new mongoose.Schema({    //Object created from object Schema class
-  email:String,
-  password:String
+/////////lEVEL-4 Hashing and 10 rounds salting the password/////////
+const userSchema = new mongoose.Schema({ //Object created from object Schema class
+  email: String,
+  password: String
 });
 
-const User = new mongoose.model("User",userSchema);
+const User = new mongoose.model("User", userSchema);
 
 
-app.get("/",function(req , res){
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/login",function(req , res){
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.get("/register",function(req , res){
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.post("/register",function(req , res){
-  const newUser = new User({
-    email:req.body.username,
-    password:md5(req.body.password)
+app.post("/register", function(req, res) {
+  cbcrypt.hash(req.body.password, saltRound, function(err, hash) { //Level - 4
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
-  newUser.save(function(err){
-    if(err){
+});
+
+app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
+    if (err) {
       console.log(err);
-    }else{
-      res.render("secrets");
+    } else {
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("secrets");
+          } else {
+            console.log("password didn't match");
+          }
+        });
+      }
     }
   });
 });
 
-app.post("/login",function(req , res){
-  const username = req.body.username;
-  const password =md5(req.body.password);
-
-  User.findOne({email:username},function(err , foundUser){
-    if(err){
-      console.log(err);
-    }else{
-      if(foundUser){
-        if(foundUser.password===password){
-          res.render("secrets");
-        }
-      }
-    }
-  })
-})
 
 
-
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
